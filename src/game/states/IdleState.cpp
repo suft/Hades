4d16c88966d7c67810b9269324e0ca6d34a89a6e
Sufien Tout <sufientout@gmail.com>
2018-01-09 05:05:12 -0500

    Utilize Menu View in Idle Game State

--- src/game/states/IdleState.cpp
+++ src/game/states/IdleState.cpp
@@ -4,6 +4,54 @@ namespace sufy { namespace game { namespace states {
 
     IdleState::IdleState(sufy::state::StateMachine<sf::RenderWindow> &machine, sf::RenderWindow &adapter, bool replace): State<sf::RenderWindow>(machine, adapter, replace) {
         this->background = sf::Color::Black;
+        std::string names[] = {"Title", "Play", "Options", "Quit"};
+        sufy::graphics::ActionColours colours = {
+            {
+                sf::Color::Green,
+                sf::Color::Red
+            },
+            {
+                sf::Color::Blue,
+                sf::Color::Red
+            },
+            {
+                sf::Color::Yellow,
+                sf::Color::Red
+            }
+        };
+
+        sf::Text text = sufy::utils::Text(
+                "Sufy",
+                *sufy::utils::Codex::AcquireFont("Polya.otf"),
+                200.0f,
+                3.5,
+                sf::Color::Cyan,
+                sf::Color::Magenta
+        );
+
+        this->menu.add(std::shared_ptr<sufy::window::gui::TextView>(new sufy::window::gui::TextView(
+                300,
+                text
+        )));
+
+        for (int i = 0; i < 4; ++i) {
+            sf::Text temp = sufy::utils::Text(
+                    names[i],
+                    *sufy::utils::Codex::AcquireFont("Polya.otf"),
+                    100.0f,
+                    1.5f,
+                    sf::Color::Green,
+                    sf::Color::Red
+            );
+            this->menu.add(std::shared_ptr<sufy::window::gui::Button>(new sufy::window::gui::Button(
+                    500 + (i * 110),
+                    temp,
+                    colours,
+                    [](){}
+            )));
+        }
+
+        this->menu.finalize();
     }
 
     void IdleState::pause() {}
@@ -12,10 +60,12 @@ namespace sufy { namespace game { namespace states {
     void IdleState::update(float dt) {
         sf::Event event;
         while (this->adapter.pollEvent(event)) this->handleEvents(event);
+        this->menu.update(dt);
     }
 
     void IdleState::render() {
         this->adapter.clear(this->background);
+        this->menu.render(this->adapter);
         this->adapter.display();
     }
 
@@ -24,6 +74,11 @@ namespace sufy { namespace game { namespace states {
             case sf::Event::Closed:
                 this->machine.quit();
                 break;
+            case sf::Event::KeyReleased:
+                menu.handleInput(event);
+                break;
+            case sf::Event::JoystickButtonPressed:
+                menu.handleInput(event);
             default:
                 break;
         }
--- src/game/states/IdleState.hpp
+++ src/game/states/IdleState.hpp
@@ -1,14 +1,22 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include "../../constant/Flags.hpp"
 #include "../../state/State.hpp"
 #include "../../state/StateMachine.hpp"
+#include "../../window/gui/TextView.hpp"
+#include "../../utils/Misc.hpp"
+#include "../../utils/Codex.hpp"
+#include "../../window/gui/Menu.hpp"
+#include "../../window/gui/Button.hpp"
+#include "../../graphics/ActionColours.hpp"
 
 namespace sufy { namespace game { namespace states {
 
     class IdleState : public sufy::state::State<sf::RenderWindow> {
     private:
         sf::Color background;
+        sufy::window::gui::Menu menu;
     public:
         IdleState(sufy::state::StateMachine<sf::RenderWindow> &machine, sf::RenderWindow &adapter, bool replace);
 
