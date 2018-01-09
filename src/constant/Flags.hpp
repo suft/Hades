1ad0595141189b9f282ce94012675a9c400e3f87
Sufien Tout <sufientout@gmail.com>
2018-01-09 05:00:18 -0500

    Added Constant Colours and Gamepad Button Mapping

--- src/constant/Flags.hpp
+++ src/constant/Flags.hpp
@@ -18,6 +18,28 @@ namespace sufy { namespace constant {
     static const sf::Vector2f ORIGIN = sf::Vector2f(0.0f, 0.0f);
     static const sf::Vector2f CENTER = sf::Vector2f((float) WIDTH/2, (float) HEIGHT/2);
     static const float DEBUG_THICKNESS = 1.0f;
-    static const sf::Color DEBUG_OUTLINE = sf::Color::Red;
+    static const sf::Color DEBUG_OUTLINE = sf::Color::White;
     static const sf::Color DEBUG_FILL = sf::Color::Transparent;
+
+    static const sf::Color LIGHTGRAY = sf::Color(211, 211, 211);
+    static const sf::Color SLATEGREY = sf::Color(112, 128, 144);
+    static const sf::Color GREY = sf::Color(128, 128, 128);
+    static const sf::Color DIMGREY = sf::Color(105, 105, 105);
+    static const sf::Color DARKSLATEGREY = sf::Color(47, 79, 79);
+
+    static const unsigned int A_BUTTON = 0;
+    static const unsigned int B_BUTTON = 1;
+    static const unsigned int X_BUTTON = 2;
+    static const unsigned int Y_BUTTON = 3;
+    static const unsigned int LB_BUTTON = 4;
+    static const unsigned int RB_BUTTON = 5;
+    static const unsigned int LEFT_JOYSTICK_BUTTON = 6;
+    static const unsigned int RIGHT_JOYSTICK_BUTTON = 7;
+    static const unsigned int START_BUTTON = 8;
+    static const unsigned int BACK_BUTTON = 9;
+    static const unsigned int HOME_BUTTON = 10;
+    static const unsigned int UP_BUTTON = 11;
+    static const unsigned int DOWN_BUTTON = 12;
+    static const unsigned int LEFT_BUTTON = 13;
+    static const unsigned int RIGHT_BUTTON = 14;
 }}
