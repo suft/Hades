c299cb72c0175f8fb22e4eaf11647b4fee522afc
Sufien Tout <sufientout@gmail.com>
2018-01-11 19:21:37 -0500

    Added AcquireTextures to load images

--- src/utils/Codex.cpp
+++ src/utils/Codex.cpp
@@ -3,6 +3,7 @@
 namespace sufy { namespace utils {
 
     std::unordered_map<std::string, std::shared_ptr<sf::Font>> Codex::fonts;
+    std::unordered_map<std::string, std::shared_ptr<sf::Texture>> Codex::textures;
 
     std::shared_ptr<sf::Font> Codex::AcquireFont(const std::string& name) {
         const auto i = fonts.find(name);
@@ -12,6 +13,17 @@ namespace sufy { namespace utils {
             font->loadFromFile(name);
             fonts.insert({name, font});
             return font;
+        }
+    }
+
+    std::shared_ptr<sf::Texture> Codex::AcquireTexture(const std::string& name) {
+        const auto i = textures.find(name);
+        if (i != textures.end()) return i->second;
+        else {
+            auto texture = std::make_shared<sf::Texture>();
+            texture->loadFromFile(name);
+            textures.insert({name, texture});
+            return texture;
         }
     }
 }}
--- src/utils/Codex.hpp
+++ src/utils/Codex.hpp
@@ -4,13 +4,16 @@
 #include <memory>
 #include <unordered_map>
 #include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Texture.hpp>
 
 namespace sufy { namespace utils {
 
     class Codex {
     private:
         static std::unordered_map<std::string, std::shared_ptr<sf::Font>> fonts;
+        static std::unordered_map<std::string, std::shared_ptr<sf::Texture>> textures;
     public:
         static std::shared_ptr<sf::Font> AcquireFont(const std::string& name);
+        static std::shared_ptr<sf::Texture> AcquireTexture(const std::string& name);
     };
 }}
