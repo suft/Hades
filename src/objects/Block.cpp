abb40622603f328eca3738536853c20c4564a40a
Sufien Tout <sufientout@gmail.com>
2018-08-22 01:11:49 -0400

    Added Tiled Map Parser

---
+++ src/objects/Block.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sufien Tout on 2018-08-18.
+//
+
+#include "Block.hpp"
---
+++ src/objects/Block.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Sufien Tout on 2018-08-18.
+//
+
+#ifndef SUFY_BLOCK_HPP
+#define SUFY_BLOCK_HPP
+
+
+class Block {
+
+};
+
+
+#endif //SUFY_BLOCK_HPP
